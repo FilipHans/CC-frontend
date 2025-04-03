84d62f5ac2ab893a88eebd1e653ad75beca61b71
stalstierna <andrea.stalstierna@chasacademy.se>
2025-04-03 11:10:16 +0200

    skelettet för formen

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -4,22 +4,26 @@ import { today, getLocalTimeZone } from "@internationalized/date";
 import { Accordion, AccordionItem } from "@heroui/react";
 
 export const HeroAccordion = () => {
-    const defaultContent = "big man";
+  const defaultContent = "big man";
 
-return (
+  return (
     <>
-    <Accordion>
+      <Accordion>
         <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-            {defaultContent}
+          {defaultContent}
         </AccordionItem>
         <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-            {defaultContent}
+          {defaultContent}
         </AccordionItem>
         <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-            {defaultContent}
+          {defaultContent}
         </AccordionItem>
-    </Accordion>
-        <Calendar isReadOnly aria-label="Date (Read Only)" value={today(getLocalTimeZone())} />
+      </Accordion>
+      <Calendar
+        isReadOnly
+        aria-label="Date (Read Only)"
+        value={today(getLocalTimeZone())}
+      />
     </>
-);
+  );
 };
